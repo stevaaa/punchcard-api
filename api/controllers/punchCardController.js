911449679f3afb98aa5084cc1999e47848137be6
Stephen Samuel <stephensamuel@Stephens-MacBook-Pro.local>
2017-10-13 13:57:34 +0200

    add actions to passphrase

--- api/controllers/punchCardController.js
+++ api/controllers/punchCardController.js
@@ -14,7 +14,7 @@ exports.get_pass_phrase = function(req, res) {
 };
 
 exports.create_pass_phrase = function(req, res) {
-	console.log(req);
+	console.log(req.body);
   var new_passphrase = new PassPhrase(req.body);
   new_passphrase.phrase = superb();
   // console.log(new_passphrase);
